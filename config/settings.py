edeee9f85ebb3dc48283cedd81208867792c1092
Ahmed Abdalla <ahmad19526@gmail.com>
2016-06-02 12:11:56 -0400

    moves hardcoded constantes to settings class

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,10 @@
 class Config(object):
+    COMMAND_QUEUE_SIZE = 500
+
     BROWSER_TO_ROBOT_TOPIC = 'browser_to_robot'
     ROBOT_TO_BROWSER_TOPIC = 'robot_to_browser'
+
+    ROUTER_REALM = 'ot_realm'
 
     ROUTER_URL = "ws://{host}:{port}/{path}".format(
         host='localhost',
