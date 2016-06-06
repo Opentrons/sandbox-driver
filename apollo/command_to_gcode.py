4f8bdefe1b2b3255f04272c5c99ff7d25dcfb9d5
Andy Sigler <andrewsigler1@gmail.com>
2016-06-06 14:58:52 -0400

    added command_to_gcode.py module with test coverage

--- apollo/command_to_gcode.py
+++ apollo/command_to_gcode.py
@@ -11,25 +11,90 @@ class CommandProcessor(object):
     def connect(self):
         yield from self.smoothie_com.connect()
 
+    @asyncio.coroutine
+    def send_gcode(self,string):
+
+        # TODO: smoothie_com.send is asynchronous, and must use 'yield from'
+        #       however, for testing, mock() is being used, so we call it synchronously
+
+        self.smoothie_com.send(string)
+
+    @asyncio.coroutine
     def process(self, command):
-        if command['type'] == 'move':
-            data = command.get('data', {})
 
-            args = ['X', 'Y', 'Z', 'a', 'b']
+        com_type = command.get('type', None)
+        com_data = command.get('data', {})
 
-            gcode = ''
+        if (com_type=='move' or com_type=='move_to') and com_data:
 
-            for arg in args:
-                gcode += (arg + '')
+            coord_types = {
+                'move' : 'G91',
+                'move_to' : 'G90'
+            }
 
-            gcode = (
-                'G0 ' +
+            axis = ['X', 'Y', 'Z', 'a', 'b']
 
-                ' '.join(
-                    ["{}{}".format(str(axis), str(val)) for axis, val in data.items()
-                ])
-            )
+            gcode = 'G0'
 
-            self.smoothie_com.send('G90')
-            self.smoothie_com.send(gcode)
+            for ax in axis:
+                # accept both lower and upper case axis
+                val = com_data.get(ax.lower()) if com_data.get(ax.lower())!=None else com_data.get(ax.upper())
+                
+                if val!=None:
+                    gcode += ' '                    # ascii space
+                    gcode += ax                     # axis
+                    gcode += str(val)               # value
+
+            yield from self.send_gcode(coord_types[com_type])   # relative
+            yield from self.send_gcode(gcode)
+
+        elif com_type=='speed' and com_data:
+
+            if com_data.get('xyz') != None:
+                yield from self.send_gcode('F{0}'.format(com_data['xyz']))
+            if com_data.get('a') != None:
+                yield from self.send_gcode('a{0}'.format(com_data['a']))
+            if com_data.get('b') != None:
+                yield from self.send_gcode('b{0}'.format(com_data['b']))
+
+        elif com_type=='acceleration' and com_data:
+
+            gcode = 'M204'
+
+            if com_data['xy'] != None:
+                gcode += ' S{}'.format(com_data['xy'])
+            if com_data['z'] != None:
+                gcode += ' Z{}'.format(com_data['z'])
+            if com_data['a'] != None:
+                 gcode += ' A{}'.format(com_data['a'])
+            if com_data['b'] != None:
+                 gcode += ' B{}'.format(com_data['b'])
+
+            if gcode!='M204':
+                yield from self.send_gcode(gcode)
+
+        elif com_type=='home':
+
+            gcode = 'G28'
+            
+            if com_data:
+                for n in com_data:
+                    gcode += ' '
+                    gcode += n.upper()
+
+            yield from self.send_gcode(gcode)
+
+        elif com_type=='hardstop':
+
+            yield from self.send_gcode('M112') # halt the motor driver
+            yield from self.send_gcode('M999') # then reset it
+
+
+
+
+
+
+
+
+
 
--- tests/test_command_to_gcode.py
+++ tests/test_command_to_gcode.py
@@ -1,18 +1,54 @@
 import unittest
 from unittest import mock
-
+import asyncio
 
 from apollo.command_to_gcode import CommandProcessor
 
 class CommandProcessorTestCase(unittest.TestCase):
+
+
+    def setUp(self):
+
+        self.loop = asyncio.new_event_loop()
+
+        self.processor = CommandProcessor()
+
+        self.processor.smoothie_com.send = mock.Mock()
+
+
     def test_move_command(self):
-        processor = CommandProcessor()
+        '''
+        Testing the 'move' command for absolute coordinates
+        '''
 
-        processor.smoothie_com = mock.Mock()
-        processor.smoothie_com.send = mock.Mock()
-
-        processor.process({
+        self.loop.run_until_complete(self.processor.process({
             'type':'move',
+            'data': {
+                'x': 100.123,
+                'y': 200.234,
+                'z': -300.987,
+                'a': 10.011,
+                'b': 20.12
+            }
+        }))
+
+        result = self.processor.smoothie_com.send.call_args_list
+
+        expected = [
+            mock.call('G91'),
+            mock.call('G0 X100.123 Y200.234 Z-300.987 a10.011 b20.12'),
+        ]
+
+        self.assertEquals(expected, result)
+
+
+    def test_move_to_command(self):
+        '''
+        Testing the 'move_to' command for relative coordinates
+        '''
+
+        self.loop.run_until_complete(self.processor.process({
+            'type':'move_to',
             'data': {
                 'x': 100,
                 'y': 200,
@@ -20,15 +56,141 @@ class CommandProcessorTestCase(unittest.TestCase):
                 'a': 10,
                 'b': 20
             }
-        })
+        }))
 
-        result = processor.smoothie_com.send.call_args_list
+        result = self.processor.smoothie_com.send.call_args_list
 
         expected = [
             mock.call('G90'),
             mock.call('G0 X100 Y200 Z300 a10 b20'),
         ]
 
-        self.assertEqual(result, expected)
+        self.assertEquals(expected, result)
+
+
+    def test_speed_command(self):
+        '''
+        Testing the 'speed' command
+        '''
+
+        self.loop.run_until_complete(self.processor.process({
+            'type':'speed',
+            'data': {
+                'xyz': 3000,
+                'y': 200,       # should be ignored
+                'z': 300,       # should be ignored
+                'a': 400,
+                'b': 500
+            }
+        }))
+
+        result = self.processor.smoothie_com.send.call_args_list
+
+        expected = [
+            mock.call('F3000'),
+            mock.call('a400'),
+            mock.call('b500'),
+        ]
+
+        self.assertEquals(expected, result)
+
+
+    def test_acceleration_command(self):
+        '''
+        Testing the 'acceleration' command
+        '''
+
+        self.loop.run_until_complete(self.processor.process({
+            'type':'acceleration',
+            'data': {
+                'xy': 3000,
+                'z': 200,
+                'x': 300,       # should be ignored
+                'a': 400,
+                'b': 500
+            }
+        }))
+
+        result = self.processor.smoothie_com.send.call_args_list
+
+        expected = [
+            mock.call('M204 S3000 Z200 A400 B500')
+        ]
+
+        self.assertEquals(expected, result)
+
+
+    def test_home_command(self):
+        '''
+        Testing the 'home' command
+        '''
+
+        expected = []
+
+        # test ALL axis
+        self.loop.run_until_complete(self.processor.process({
+            'type':'home',
+            'data': ['x','y','z','a','b']
+        }))
+
+        result = self.processor.smoothie_com.send.call_args_list
+
+        expected.append(mock.call('G28 X Y Z A B'))
+
+        self.assertEquals(expected, result)
+
+        # test a few of the axis
+        self.loop.run_until_complete(self.processor.process({
+            'type':'home',
+            'data': ['x','b']
+        }))
+
+        result = self.processor.smoothie_com.send.call_args_list
+
+        expected.append(mock.call('G28 X B'))
+
+        self.assertEquals(expected, result)
+
+        # test if data is None
+        self.loop.run_until_complete(self.processor.process({
+            'type':'home',
+            'data': None
+        }))
+
+        result = self.processor.smoothie_com.send.call_args_list
+
+        expected.append(mock.call('G28'))
+
+        self.assertEquals(expected, result)
+
+
+    def test_hardstop_command(self):
+        '''
+        Testing the 'hardstop' command
+        '''
+
+        self.loop.run_until_complete(self.processor.process({
+            'type':'hardstop'
+        }))
+
+        result = self.processor.smoothie_com.send.call_args_list
+
+        expected = [
+            mock.call('M112'),
+            mock.call('M999')
+        ]
+
+        self.assertEquals(expected, result)
+
+
+
+
+
+
+
+
+
+
+
 
 
