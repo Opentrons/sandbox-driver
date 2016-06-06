b01d9b0a026e2384fc4b4ca4ab3128822248036a
Ahmed Abdalla <ahmad19526@gmail.com>
2016-06-06 15:51:19 -0400

    refactors command processor component; updates startup script

--- apollo/command_processor_component.py
+++ apollo/command_processor_component.py
@@ -3,6 +3,7 @@
 import asyncio
 import logging
 import multiprocessing
+import queue
 
 from autobahn.asyncio import wamp
 
@@ -30,12 +31,33 @@ class CommandProcessorComponent(wamp.ApplicationSession):
             raise Exception('A control_queue must be set in self.config.extra')
 
         while True:
-            id, msg = yield from utils.coro_queue_get(command_queue)
+            cnt_msg = yield from utils.coro_queue_get(control_queue)
+
+            if cnt_msg == 'pause':
+                while True:
+                    cnt_msg = yield from utils.coro_queue_get(control_queue)
+                    if cnt_msg == 'resume':
+                        break
+                    yield from asyncio.sleep(0.1)
+
+            elif cnt_msg == 'erase':
+                utils.flush_queue(command_queue)
+                continue
+
+            elif cnt_msg == 'resume':
+                pass
+
+            else:
+                logger.error('Received unknown control message: {}'.format(cnt_msg))
+                break
+
+            cmd_pkt = yield from utils.coro_queue_get(command_queue)
+            pkt_id = cmd_pkt.get('id', 'ID NOT FOUND')
 
             # TODO: execute roboto message and publish result
 
-            logger.debug('Dequeued Message ID: {} with {}'.format(id, msg))
-            self.publish(Config.ROBOT_TO_BROWSER_TOPIC, msg)
+            logger.debug('Dequeued Message ID: {} with {}'.format(pkt_id, cmd_pkt))
+            self.publish(Config.ROBOT_TO_BROWSER_TOPIC, cmd_pkt)
 
 
 if __name__ == '__main__':
@@ -48,11 +70,15 @@ if __name__ == '__main__':
     )
 
     command_queue = multiprocessing.Manager().Queue(50)
+    control_queue = multiprocessing.Manager().Queue(50)
 
     print(type(command_queue))
 
     runner = wamp.ApplicationRunner(
         url, realm='ot_realm',
-        extra={'command_queue': command_queue}
+        extra={
+            'command_queue': command_queue,
+            'control_queue': control_queue
+        }
     )
     runner.run(CommandProcessorComponent)
--- apollo/utils.py
+++ apollo/utils.py
@@ -1,5 +1,6 @@
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
+import queue
 import socket
 
 
@@ -12,7 +13,17 @@ def get_free_os_address():
 
 
 @asyncio.coroutine
-def coro_queue_get(queue, loop=None):
+def coro_queue_get(q, loop=None):
     loop = loop or asyncio.get_event_loop()
-    return (yield from loop.run_in_executor(ThreadPoolExecutor(1), queue.get))
+    return (yield from loop.run_in_executor(ThreadPoolExecutor(1), q.get))
+
+def flush_queue(q):
+    items_cnt = q.qsize()
+
+    while items_cnt > 0:
+        try:
+            q.get_nowait()
+            items_cnt -= 1
+        except queue.Empty:
+            break
 
