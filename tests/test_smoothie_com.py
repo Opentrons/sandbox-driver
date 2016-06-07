e63da185af9c21e6394de8951e2981ffec1a1eee
Ahmed Abdalla <ahmad19526@gmail.com>
2016-06-07 10:41:24 -0400

    experimenting with asyncio testing

--- tests/test_smoothie_com.py
+++ tests/test_smoothie_com.py
@@ -5,6 +5,30 @@ import apollo.utils
 
 from apollo.smoothie import SmoothieCom
 
+def get_mock_coro(return_value):
+    @asyncio.coroutine
+    def mock_coro(*args, **kwargs):
+        return return_value
+
+    return mock.Mock(wraps=mock_coro)
+
+
+class Writer(object):
+    def __init__(self):
+        self.write_buffer = []
+        self.drain_buffer = []
+
+    @asyncio.coroutine
+    def write(self, data):
+        self.write_buffer.append(data)
+        print('write buffer has', self.write_buffer)
+
+    @asyncio.coroutine
+    def drain(self):
+        for item in self.write_buffer:
+            self.drain_buffer.append(item)
+        self.write_buffer = []
+
 
 class SmoothieComTest(unittest.TestCase):
     def setUp(self):
@@ -17,32 +41,34 @@ class SmoothieComTest(unittest.TestCase):
             self.address[1],
             self.loop,
         )
-        self.run_in_loop(self.smc.connect())
+        # self.run_in_loop(self.smc.connect())
 
     def run_in_loop(self, coro):
         return self.loop.run_until_complete(coro)
 
-    def tearDown(self):
-        self.run_in_loop(self.smc.writer.close())
+    def test_send_M114_gcode(self):
+        write_mock = mock.Mock()
+        writer_mock = mock.Mock()
+        drain_mock = mock.Mock()
 
-    def test_connect(self):
-        smc = SmoothieCom(
-            self.address[0],
-            self.address[1],
-            self.loop,
-            is_feedback_enabled=False
-        )
-        self.assertTrue(self.run_in_loop(smc.connect()))
+        reader_mock = mock.Mock()
+        read_mock = mock.Mock()
+        read_line_mock = mock.Mock()
 
-    # def test_send_M114_gcode(self):
-    #     write_mock = mock.Mock()
-    #
-    #     import pdb; pdb.set_trace()
-    #     self.smc.writer.write = write_mock
-    #
-    #     self.loop.run_until_complete(self.smc.send('M114'))
-    #
-    #
+        self.smc.writer = get_mock_coro(writer_mock)
+        self.smc.writer.drain = get_mock_coro(drain_mock)
+        self.smc.writer.write = get_mock_coro(write_mock)
+
+        self.smc.reader = get_mock_coro(reader_mock)
+        self.smc.reader.read = get_mock_coro(read_mock)
+        self.smc.reader.readline = get_mock_coro(read_line_mock)
+
+
+        self.loop.run_until_complete(self.smc.send('M114', None))
+
+        import pdb; pdb.set_trace()
+
+
     # def test_send_G0_gcode(self):
     #     pass
     #
