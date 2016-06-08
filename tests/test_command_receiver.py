93147efc7fc5bb394daadf4c3b0e5268a5deb918
Ahmed Abdalla <ahmad19526@gmail.com>
2016-06-08 11:32:33 -0400

    save experiments

--- tests/test_command_receiver.py
+++ tests/test_command_receiver.py
@@ -1,7 +1,14 @@
+import asyncio
 import multiprocessing
 import unittest
+from unittest import mock
 
-from apollo.command_receiver_component import enqueue_message
+from autobahn.wamp.types import ComponentConfig
+
+from apollo.command_receiver_component import (
+    enqueue_message,
+    CommandReceiverComponent
+)
 
 
 class CommandReceiverComponentTestCase(unittest.TestCase):
@@ -15,3 +22,21 @@ class CommandReceiverComponentTestCase(unittest.TestCase):
 
         self.assertEqual(msg_input, msg_output)
         self.assertTrue(isinstance(idx, str))
+
+
+    def test_command_recv(self):
+
+        command_queue = mock.Mock()
+
+        config = ComponentConfig(extra={'command_queue': command_queue})
+
+        comp = CommandReceiverComponent(config)
+        comp._transport = mock.Mock()
+
+        loop = asyncio.get_event_loop()
+
+        loop.run_until_complete(comp.onJoin(None))
+
+        import pdb; pdb.set_trace()
+
+
