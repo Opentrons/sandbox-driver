af774543890fcd4803ae13ef1b69d51cb00cbd7e
Ahmed Abdalla <ahmad19526@gmail.com>
2016-06-10 10:56:32 -0400

    removes unneccessary blank lines

--- apollo/command_to_gcode.py
+++ apollo/command_to_gcode.py
@@ -1,46 +1,41 @@
 import asyncio
 
 from apollo.smoothie import SmoothieCom
-
 from config.settings import Config
 
-class CommandToGCode(object): 
 
-    '''
+class CommandToGCode(object): 
+    """
     this module converts front-end commands to GCode
     then sends GCode strings to Smoothie-Com module
-    '''
+    """
 
     def __init__(self):
-
         self.smoothie_com = SmoothieCom(Config.SMOOTHIE_URL, Config.SMOOTHIE_PORT)
-
 
     @asyncio.coroutine
     def connect(self):
-        '''
+        """
         tell the smoothie module to connect to the motor controller
         return True if successful
-        '''
+        """
         return (yield from self.smoothie_com.connect())
-
 
     @asyncio.coroutine
     def send_gcode(self,string):
-        '''
+        """
         send a GCode string to the smoothie module
-        '''
+        """
         if len(string):
             return (yield from self.smoothie_com.send(string))
 
-
     @asyncio.coroutine
     def process(self, command):
-        '''
+        """
         receive front-end commands, and convert them to GCode
         returns None or a coordinate dict
-        '''
-        
+        """
+
         # check to see if this module has a method matching the command's 'type'
         method = getattr(self, command.get('type', '') , None)
 
@@ -58,12 +53,11 @@ class CommandToGCode(object):
 
             return coords
 
-
     def parse_coordinates(self,coords):
-        '''
+        """
         removes unnecessary data from the coordinates object returned from smoothie-com
         returns a dict
-        '''
+        """
 
         if type(coords) is dict:
 
@@ -75,13 +69,11 @@ class CommandToGCode(object):
 
             return new_coords
 
-
     def create_gcode_string(self,data,type):
-
-        '''
+        """
         turns a front-end command dict into the equivalent gcode string
         returns a string
-        '''
+        """
 
         temp_string = ''
 
@@ -96,71 +88,58 @@ class CommandToGCode(object):
         else:
             return ''
 
-
     def move(self,data):
-
-        '''
+        """
         create absolute or relative movement gcode
         returns array of strings
-        '''
+        """
 
         mode = Config.GCODE_COMMANDS['move_rel'] if data.get('relative',False) else Config.GCODE_COMMANDS['move_abs']
 
         return [ mode , self.create_gcode_string(data,'move') ]
 
-
     def position(self,data):
-
-        '''
+        """
         GET or SET the current axis positions
         returns array of strings
-        '''
+        """
 
         if data and type(data) is dict and len(data.keys()):
             return [ self.create_gcode_string(data,'position') ]
         else:
             return [ Config.GCODE_COMMANDS['position_get'] ]
 
-
     def speed(self,data):
-
-        '''
+        """
         create 'speed' gcode
         returns array of strings
-        '''
+        """
 
         return [ self.create_gcode_string(data,'speed') ]
 
-
     def acceleration(self,data):
-        '''
+        """
         create 'acceleration' gcode
         returns array of strings
-        '''
+        """
 
         return [ self.create_gcode_string(data,'acceleration') ]
 
-
     def home(self,data):
-
-        '''
+        """
         create 'home' gcode: if no axis are specified, all axis are homed at once
         returns array of strings
-        '''
+        """
 
         temp_string = Config.GCODE_COMMANDS['home']
-            
         for n in (data if data else []): # a None might be passed
             temp_string += ' {}'.format(str(n[0]).upper()) # UPPER CASE
 
         return [ temp_string ]
 
-
     def hardstop(self,data):
-        '''
+        """
         create 'hardstop' gcode: halt the motor driver, then reset it
         returns array of strings
-        '''
+        """
         return [ Config.GCODE_COMMANDS['hardstop'] , Config.GCODE_COMMANDS['reset'] ]
-
-
